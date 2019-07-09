fd337da86743c476b00ec84f4b07817c8056edcf
thaweesak <ab0987079646@gmail.com>
2019-07-09 11:21:09 +0700

    lab1 basic java

---
+++ src/Lab1/Exercies.java
@@ -0,0 +1,25 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class Exercies {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("กรอกค่าฐาน: ");
+        int x = scanner.nextInt();
+
+        System.out.print("กรอกค่าความสุง: ");
+        int y = scanner.nextInt();
+
+        System.out.println("พื้นที่สามเหลี่ยมเท่ากับ"+(0.5*x*y));
+
+
+
+
+
+
+    }//main
+}//class
---
+++ src/Lab1/Intro.java
@@ -0,0 +1,25 @@
+package Lab1;
+
+public class Intro {
+
+    public static void main(String[] args) {
+        System.out.println("hello oop 2019");
+System.out.print("holle oop2019. \n");
+System.out.print("\tholle oop2019\n");
+
+//integer
+        int x;
+        x = 10;
+        int y = 20;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(x+y);
+        System.out.println("the summation of x and y = "+(x+y));
+
+
+
+    }//main
+
+
+}//class
