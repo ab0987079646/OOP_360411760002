956832672e70053e083a1a0686ac8de4d8f8e624
thaweesak <ab0987079646@gmail.com>
2019-07-23 11:24:25 +0700

    lab2 > lab_1 lab_2 lab_3.ex1

---
+++ src/Lab2/Lab_3.java
@@ -0,0 +1,19 @@
+package Lab2;
+
+import java.util.Scanner;
+
+public class Lab_3 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("กรอกค่า fAHRENHEIT");
+        double a =scanner.nextDouble();
+
+        System.out.println("celsius " +(5.0/9.0*a-32.0));
+
+
+    }//main
+}//class
---
+++ src/Lab2/lab_2.java
@@ -0,0 +1,32 @@
+package Lab2;
+
+import java.util.Scanner;
+
+public class lab_2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+     System.out.println("กรุณากรอก ชื่อ - สกุล");
+     String a = scanner.nextLine();
+
+        System.out.println("กรุณากรอกเลชบัตรประชาชน=");
+        String b = scanner.nextLine();
+
+        System.out.println("กรุณากรอก ที่อยู่");
+        String c = scanner.nextLine();
+
+        System.out.println("กรุณากรอก อีเมลล์");
+        String d = scanner.nextLine();
+
+        System.out.println("หมายเลขโทรศัพท์");
+        String e = scanner.nextLine();
+
+        System.out.println(a );
+        System.out.println(b );
+        System.out.println(c );
+        System.out.println(d );
+        System.out.println(e );
+    }//main
+}//class
